5193068fa6fca1184eac874cd0d8f9ec0ed69d0e
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2019-05-10 08:34:34 +0200

    Fix data manager to allow updating static db

--- src/carscanner/data/data.py
+++ src/carscanner/data/data.py
@@ -6,16 +6,17 @@ from .readonly import ReadOnlyMiddleware
 
 
 class DataManager:
-    def __init__(self, data_dir: pathlib.Path):
+    def __init__(self, data_dir: pathlib.Path, static_rw=False):
         self._static_data_path = data_dir / 'static.json'
         self._cars_data_path = data_dir / 'cars.json'
+        self._static_rw = static_rw
         self._static: tinydb.TinyDB = None
         self._cars: tinydb.TinyDB = None
         self._mem: tinydb.TinyDB = None
 
     def static_data(self) -> tinydb.TinyDB:
         if self._static is None:
-            storage = ReadOnlyMiddleware()
+            storage = tinydb.TinyDB.DEFAULT_STORAGE if self._static_rw else ReadOnlyMiddleware()
             self._static = tinydb.TinyDB(storage=storage, path=self._static_data_path, indent=2)
         return self._static
 
